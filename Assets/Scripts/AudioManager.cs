ea1f99f017455d73a29dd1fc8ace6e47d42e0cb3
Ronan <30617930+MWRonanD@users.noreply.github.com>
2021-01-07 20:50:53 +0100

    Final Commit

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+}
